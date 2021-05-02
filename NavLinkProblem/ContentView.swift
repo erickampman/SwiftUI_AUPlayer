38a9ac78be614b66e6db23af235bfc52d72668b9
Eric Kampman <erickampman@yahoo.com>
2021-05-02 09:00:17 -0700

    Initial Commit

---
+++ NavLinkProblem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavLinkProblem
+//
+//  Created by Eric Kampman on 5/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavLinkProblem/NavLinkProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavLinkProblemApp.swift
+//  NavLinkProblem
+//
+//  Created by Eric Kampman on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct NavLinkProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
